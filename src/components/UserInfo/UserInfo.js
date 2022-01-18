1b352ee660d8593eda97c71766766f36d0320a07
DaLast1 <dalast1@gmail.com>
2022-01-18 15:18:39 +0700

    fix a lil display

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -6,12 +6,15 @@ import BoxTuyChon from './ChildComponent/BoxTuyChon';
 import BoxThoiGian from './ChildComponent/BoxThoiGian';
 import BoxContentTabMain from './ChildComponent/BoxContentTabMain';
 import { ThemeContext } from '../../shared/context/ThemeProvider';
+import { AuthContext } from '../../shared/context/auth-context';
 
 import Cart from '../Cart';
 
 import './UserInfo.css';
 
 function UserInfo() {
+  const auth = useContext(AuthContext);
+
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   const [showBoxThoiGian, setShowBoxThoiGian] = useState(false);
@@ -121,7 +124,7 @@ function UserInfo() {
           >
             <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
           </svg>
-          <span className="nameUser">User Demo</span>
+          {auth.username && <span className="nameUser">{auth.username}</span>}
           <div
             className="camera"
             onClick={() => setShowBoxAnhDaiDien((pre) => !pre)}
